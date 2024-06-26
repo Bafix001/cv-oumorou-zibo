339578913fb8eae37dad7506cf2d2847e6e34cb5
Bafix <Bafix@example.com>
2024-06-26 13:03:48 +0200

    Ajout d'un fichier de workflow pour le déploiement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
-// https://astro.build/config
+// Supprimez une des importations de defineConfig
+// Vous n'avez besoin que d'une seule déclaration d'importation de defineConfig
+
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
-  output: 'static'
+  site: "https://Bafix001.github.io",
 });
