26db15cac74f87fba57892f3da5a3b2c6feed28b
Bafix <Bafix@example.com>
2024-06-26 12:03:36 +0200

    first commit

---
+++ src/pages/content.js
@@ -0,0 +1,176 @@
+// Personal Information --> #hero section
+export const name = "Oumorou ZIBO";
+export const designation = "Etudiant en master 1 (Msc pro). ";
+export const location = "Marseille, France";
+// export const pronouns = "he/him";
+export const mail = "zibooumorou44@gmail.com";
+
+// About
+export const about =
+  "Enchanté de vous rencontrer ! Je suis passionné par le croisement entre la gestion de projet et l'informatique, un domaine où je me suis pleinement investi après avoir obtenu ma licence en gestion de projet en 2018. Ma transition vers l'informatique s'est concrétisée par l'obtention d'une licence en MIAGE à Aix-Marseille, une étape qui a enrichi mon parcours d'une expertise solide en systèmes d'information et en gestion des données." +
+  "Aujourd'hui, je me lance avec enthousiasme dans la prochaine étape de mon parcours académique : la recherche d'une opportunité d'alternance pour compléter mon cursus par un master en informatique. Mon ambition est d'approfondir mes connaissances dans les technologies émergentes et leur application pratique, tout en contribuant de manière significative aux projets innovants au sein de votre entreprise." +
+  "Ma motivation réside dans la capacité de transformer les défis en opportunités, d'innover avec rigueur et d'apporter une réelle valeur ajoutée à chaque projet auquel je contribue. Je suis convaincu que mon parcours diversifié et ma passion pour l'apprentissage continu sont des atouts précieux pour relever les défis complexes de l'informatique moderne." +
+  "En dehors de ma vie professionnelle, j'apprécie les défis sportifs, la lecture et la découverte de nouvelles cultures. Ces passions alimentent ma créativité et ma détermination à exceller dans tous les aspects de ma vie.";
+
+// Work Experience --> #work section
+export const work = [
+  {
+    from: "2022",
+    to: "Present",
+    role: "Développement de Projets Personnels",
+    org: "Etudiant",
+    // org_url: "https://quahog.gov/safety",
+    location: "Aix-en-Provence, France",
+    skills: "Développement de Projets, Programmation, Gestion de Projet",
+  },
+
+  {
+    from: "Avril 2023",
+    to: "Août 2023",
+    role: "Stagiaire en développement full stack",
+    org: "Parcs Enchères",
+    // org_url: "https://quahog.gov/safety",
+    location: "Aubagne, France",
+    skills: "Analyse des données, Développement Front-end, Intégration API",
+    description:
+      "Participation à la conception et à l'implémentation d'un système d'estimation de la valeur des voitures d'occasion, incluant le développement de fonctionnalités clés pour les produits logiciels de l'entreprise.",
+  },
+
+  {
+    from: "Juillet 2021",
+    to: "Septembre 2021",
+    role: "Stagiaire en développement web",
+    org: "Horions Communication",
+    location: "Cotonou, Bénin",
+    skills:
+      "Conception de sites web, Développement Frontend et Backend, Technologies Web",
+    description:
+      "Apport à la conception et à la réalisation de sites web pour la clientèle de l'entreprise, en exploitant les technologies frontend et backend.",
+  },
+];
+
+// Projects --> #project section
+export const projects = [
+  {
+    from: "2023",
+    to: "2023",
+    title: "Wine or Die",
+    description:
+      "Projet de développement en Java pour un jeu de console où les joueurs doivent naviguer à travers des énigmes et des défis pour survivre dans un monde dystopique.",
+
+    tech: "Java, Console Application, Développement de Jeux",
+  },
+
+  {
+    from: "2023",
+    to: "Présent",
+    title: "Gestion des Notes Scolaires",
+    description:
+      "Application Java développée en Spring Boot pour gérer les notes des élèves et leur gestion administrative, avec une interface web basée sur Spring Web. Le projet est continuellement amélioré pour intégrer de nouvelles fonctionnalités basées sur des idées innovantes.",
+
+    tech: "Spring Boot, Spring Web, Java, Gestion de Base de Données",
+  },
+];
+
+// Education --> #education section
+export const education = [
+  {
+    from: "2024",
+    to: "présent",
+    institute: "Epitech",
+    course: "Msc Pro (Master 1)",
+    location: "Marseille, France",
+  },
+  {
+    from: "2002",
+    to: "2024",
+    institute: "Aix Marseille Université",
+    course:
+      "Licence MIAGE ( Méthode Informatique Appliquée à la Gestion des Entreprises",
+    location: "Aix en Provence, Marseille ",
+  },
+  {
+    from: "2015",
+    to: "2018",
+    institute: "Cours Sonou Université",
+    course: "Licence Création d'Entreprise et Gestion de Projet",
+    location: "Cotonou, Bénin",
+  },
+];
+
+// Contact --> #contact section
+export const contact = [
+  {
+    source_name: "zibooumorou44@gmail.com",
+    // source: "zibooumorou44@gmail.com",
+  },
+  {
+    source_name: "Twitter",
+    source: "https://twitter.com/Omar_Bafix",
+  },
+  {
+    source_name: "LinkedIn",
+    source: "https://www.linkedin.com/in/oumorou-zibo-761747204/",
+  },
+];
+
+// Certifications --> #certificate section
+
+export const certificates = [
+  {
+    issued: "2021",
+    title: "Certificat en Développement Web",
+    org: "Savoir Plus School ",
+    // url: "https://certi.com/url",
+    description:
+      "Formation certifiante en PHP, HTML et CSS, acquérant une expertise essentielle pour le développement web. Compétences renforcées dans la création dynamique de sites web avec PHP, ainsi que dans la structuration et le stylisme des interfaces avec HTML et CSS.",
+  },
+];
+
+// Blogs --> #blogs section
+export const blogs = [
+  {
+    // date: "12-July, 2023",
+    // url: "https://medium.com/",
+    // title: "Adventures in Safety Inspection",
+    // description:
+    //   "Join me in my humorous escapades as a safety inspector in Quahog.",
+    // publisher: "Quahog Times",
+  },
+  {
+    // date: "25-June, 2023",
+    // url: "https://medium.com/",
+    // title: "Top 10 Safety Tips for Families",
+    // description:
+    //   "Learn the best safety practices for families to ensure a secure environment.",
+    // publisher: "Safety Monthly",
+  },
+];
+
+// Achievements --> #achievement section
+export const achievements = [
+  {
+    title: "Safety Hero Award",
+    year: "2020",
+    decription:
+      "Recognized for outstanding contributions to improving safety standards in Quahog.",
+  },
+  {
+    title: "Safety Innovation Prize",
+    year: "2018",
+    decription:
+      "Awarded for implementing innovative safety measures that yielded significant improvements.",
+  },
+  {
+    title: "Community Safety Ambassador",
+    year: "2015",
+    decription:
+      "Honored for promoting safety awareness and education in the Quahog community.",
+  },
+  {
+    title: "Safety Campaign Excellence",
+    year: "2013",
+    decription:
+      "Acknowledged for leading a successful safety campaign that reached thousands of residents.",
+  },
+];
